1d639e68c444d901d971f172293b6b16ac7d2689
kshshton <96781803+kshshton@users.noreply.github.com>
2026-07-13 13:01:26 +0000

    [ds][list] Update capacity logic

--- data_structures/list.c
+++ data_structures/list.c
@@ -23,7 +23,7 @@ int get(List* list, int idx) {
         fprintf(stderr, "Invalid argument!\n");
         return 1;
     }
-    
+
     return list->data[idx];
 }
 
@@ -31,42 +31,51 @@ void set(List* list, int idx, int value) {
     if (idx < 0 || idx > list->size - 1) {
         fprintf(stderr, "Invalid argument!\n");
     }
-    
+
     list->data[idx] = value;
+}
+
+void double_capacity(List* list) {
+    list->capacity *= 2;
+    int* new_data = realloc(list->data, sizeof(int) * list->capacity);
+
+    if (new_data == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    list->data = new_data;
 }
 
 void append(List* list, int value) {
     if (list->capacity == list->size) {
-        list->capacity *= 2;
-        list->data = realloc(list->data, sizeof(int) * list->capacity);
+        double_capacity(list);
     }
-    
-    list->capacity += sizeof(int);
+
     list->data[list->size] = value;
     list->size++;
 }
 
 void insert(List* list, int idx, int value) {
-    if (idx < 0) {
-        fprintf(stderr, "Invalid argument!\n");
-    }
-    
+    if (idx < 0) fprintf(stderr, "Invalid argument!\n");
+
     if (idx > list->size) idx = list->size;
-    
-    list->capacity += sizeof(int);
+
+    if (list->capacity == list->size) {
+        double_capacity(list);
+    }
+
     list->size++;
 
     for (int i = idx; i < list->size; i++) {
         int temp = list->data[idx];
         list->data[i++] = temp;
     }
-    
+
     list->data[idx] = value;
 }
 
 void pop(List* list) {
-    list->capacity -= sizeof(int);
-    list->data = realloc(list->data, sizeof(int) * list->capacity);
     list->size--;
 }
 
@@ -74,8 +83,7 @@ void remove_at(List* list, int idx) {
     for (int i = idx; i < list->size - 1; i++) {
         list->data[i] = list->data[i + 1];
     }
-    
-    list->capacity -= sizeof(int);
+
     list->size--;
 }
 
@@ -88,7 +96,7 @@ int main() {
     remove_at(list, 0); // [10, 3]
     pop(list);          // [10]
     printf("%d\n", get(list, 0));
-    
+
     for (int i = 0; i < list->size; i++) {
         printf("index = %d, value = %d, size = %d, capacity = %d\n", i, list->data[i], list->size, list->capacity);
     }

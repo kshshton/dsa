1759601a219061d248dbe7874f5757d48b126303
kshshton <96781803+kshshton@users.noreply.github.com>
2026-07-23 14:12:05 +0000

    [alg][dfs] Update comments

--- algorithms/dfs.c
+++ algorithms/dfs.c
@@ -2,10 +2,38 @@
 
 # define MAX 10
 
+// Goal: Explore as far as possible along one path before backtracking.
+
+// Creating graph:
+//        0
+//       / \
+//      1   2
+//     / \
+//   3   4
+//       |
+//       5
+
+// Matrix:
+//      0 1 2 3 4 5
+//     ----------------
+// 0 |  0 1 1 0 0 0
+// 1 |  1 0 0 1 1 0
+// 2 |  1 0 0 0 0 0
+// 3 |  0 1 0 0 0 0
+// 4 |  0 1 0 0 0 1
+// 5 |  0 0 0 0 1 0
+
+// Travesal:
+// 0 -> 1 -> 3
+//        -> 4 -> 5
+//   -> 2
+
+// Order:
+// 013452
+
 int graph[MAX][MAX];
 int vertices = MAX;
 int visited[MAX];
-
 
 void dfs(int node) {
     visited[node] = 1;
@@ -19,25 +47,6 @@ void dfs(int node) {
 
 int main()
 {
-    // Creating graph:
-    //        0
-    //       / \
-    //      1   2
-    //     / \
-    //   3   4
-    //       |
-    //       5
-    
-    // Matrix:
-    //      0 1 2 3 4 5
-    //     ----------------
-    // 0 |  0 1 1 0 0 0
-    // 1 |  1 0 0 1 1 0
-    // 2 |  1 0 0 0 0 0
-    // 3 |  0 1 0 0 0 0
-    // 4 |  0 1 0 0 0 1
-    // 5 |  0 0 0 0 1 0
-    
     graph[0][1] = graph[1][0] = 1;
     graph[0][2] = graph[2][0] = 1;
     graph[1][3] = graph[3][1] = 1;
